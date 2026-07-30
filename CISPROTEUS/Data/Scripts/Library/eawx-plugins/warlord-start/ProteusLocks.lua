1115bcb3d73362f24af24afa029bd0c8786404bd
TaxEvader6686 <garpur2911@gmail.com>
2026-07-30 17:58:45 +0000

    Event Dummy and Proteus file cleanup

--- CISPROTEUS/Data/Scripts/Library/eawx-plugins/warlord-start/ProteusLocks.lua
+++ CISPROTEUS/Data/Scripts/Library/eawx-plugins/warlord-start/ProteusLocks.lua
@@ -2,17 +2,17 @@ return {
 		--Units to lock:
 
 		--Base mod standard availble units
-		-- "Gamma_ATR_6_Group", "IPV1", "Lancer_Frigate", "Carrack_Cruiser", "Ton_Falk_Escort_Carrier", "Star_Galleon", "Strike_Cruiser",
-		-- "Imperial_DHC", "Vindicator_Cruiser", "Acclamator_II", "Immobilizer_418", "Victory_I_Star_Destroyer", "Victory_II_Star_Destroyer","Procursator_Star_Destroyer",
-		-- "Interdictor_Star_Destroyer", "Imperial_I_Star_Destroyer", "Imperial_II_Star_Destroyer", "Tector_Star_Destroyer", "Allegiance_Battlecruiser",
-		-- "Executor_Star_Dreadnought",
-		-- "Imperial_Army_Trooper_Company", "Imperial_Army_Guard_Company", "Imperial_Army_74Z_Bike_Company",
-		-- "Imperial_AT_PT_Company", "Chariot_LAV_Company", "AT_ST_Company",
-		-- "AT_AA_Walker_Company", "S_1_Firehawke_Company", "SPMAG_Walker_Company", "Deathhawk_Company", "Imperial_Dropship_Transport_Company",
-		-- "PX4_Company", "Imperial_A5_Juggernaut_Company", "Imperial_AT_AT_Walker_Company",
+		 "Gamma_ATR_6_Group", "IPV1", "Lancer_Frigate", "Carrack_Cruiser", "Ton_Falk_Escort_Carrier", "Star_Galleon", "Strike_Cruiser",
+		 "Imperial_DHC", "Vindicator_Cruiser", "Acclamator_II", "Immobilizer_418", "Victory_I_Star_Destroyer", "Victory_II_Star_Destroyer","Procursator_Star_Destroyer",
+		 "Interdictor_Star_Destroyer", "Imperial_I_Star_Destroyer", "Imperial_II_Star_Destroyer", "Tector_Star_Destroyer", "Allegiance_Battlecruiser",
+		 "Executor_Star_Dreadnought",
+		 "Imperial_Army_Trooper_Company", "Imperial_Army_Guard_Company", "Imperial_Army_74Z_Bike_Company",
+		 "Imperial_AT_PT_Company", "Chariot_LAV_Company", "AT_ST_Company",
+		 "AT_AA_Walker_Company", "S_1_Firehawke_Company", "SPMAG_Walker_Company", "Deathhawk_Company", "Imperial_Dropship_Transport_Company",
+		 "PX4_Company", "Imperial_A5_Juggernaut_Company", "Imperial_AT_AT_Walker_Company",
 
 		--Base mod specific faction unlock units
-		-- "ISB_Infiltrator_Company", "Imperial_A5RX_Company", "Dragon_Heavy_Cruiser", "Persuader_Company", "Citadel_Guardsman_Company", "Eidolon", "Lorrir_Location_Set", "Raptor_Trooper_Company", "Incinerator_Stormtrooper_Company", "Imperial_TX130S_Company", "Silverline_Location_Set", "Torpedo_Sphere", "Firebird_Stormtrooper_Company", "Shadow_Stormtrooper_Company", "Imperial_Navy_Trooper_Company", "PDF_Force_Cultist_Company", "CSA_Tagge_Battlecruiser", "TaggeCo_HQ", "Imperial_DP20", "CEC_HQ", "Customs_Corvette", "TIE_Crawler_Company", "Charger_C70", "Tarkin_Estates", "Marauder_Cruiser", "Repulsor_Tank_Mark_II_Company", "Imperial_AT_AP_Walker_Company", "Tam_Blackstar_Dark_Blade_Location_Set", "Pursuit_Light_Cruiser", "CR92A", "Deathfire_Gamma_Location_Set", "Bulwark_I", "TransGalMeg_HQ", "Raider_I_Corvette", "AT_MP_Company", "IPV1_Zeta", "Venator_Star_Destroyer",
+		 "ISB_Infiltrator_Company", "Imperial_A5RX_Company", "Dragon_Heavy_Cruiser", "Persuader_Company", "Citadel_Guardsman_Company", "Eidolon", "Lorrir_Location_Set", "Raptor_Trooper_Company", "Incinerator_Stormtrooper_Company", "Imperial_TX130S_Company", "Silverline_Location_Set", "Torpedo_Sphere", "Firebird_Stormtrooper_Company", "Shadow_Stormtrooper_Company", "Imperial_Navy_Trooper_Company", "PDF_Force_Cultist_Company", "CSA_Tagge_Battlecruiser", "TaggeCo_HQ", "Imperial_DP20", "CEC_HQ", "Customs_Corvette", "TIE_Crawler_Company", "Charger_C70", "Tarkin_Estates", "Marauder_Cruiser", "Repulsor_Tank_Mark_II_Company", "Imperial_AT_AP_Walker_Company", "Tam_Blackstar_Dark_Blade_Location_Set", "Pursuit_Light_Cruiser", "CR92A", "Deathfire_Gamma_Location_Set", "Bulwark_I", "TransGalMeg_HQ", "Raider_I_Corvette", "AT_MP_Company", "IPV1_Zeta", "Venator_Star_Destroyer",
 
 		--Custom unit entries per faction
 
@@ -24,6 +24,13 @@ return {
 		 "Imperial_Light_Mobile_Artillery_Company",
 		 "B5_Juggernaut_Company", "Yevethan_AT_AT_Walker_Company",
 
-		--Planned Corellian Proteus added units
-		--"",
+		--New Seperatist Union
+		"CSA_B1_Droid_Company", "B2_Droid_Company", "Crab_Droid_Company", "Destroyer_Droid_II_Company", "Imperial_Dwarf_Spider_Droid_Company", "STAP_Company",
+			"OG9_Company", "Hailfire_Company",
+			"AAT_Company", "HAGM_Company", "HAML_Company", "PAC_Company",
+			"Magna_Octuptarra_Company", "HMP_Company", "MTT_Company", 
+			"Pursuer_Enforcement_Ship_Group", "Diamond_Frigate", "Hardcell", "Munifex", "C9979_Carrier",
+			"Munificent", "Recusant_Light_Destroyer", "DH_Omni", "Providence_Carrier_Destroyer", "Bulwark_I", "Captor", "Auxilia",
+			"Bulwark_II", "Providence_Dreadnought",
+			"Lucrehulk_Battleship", "Lucrehulk_Carrier", "Subjugator",
 }
